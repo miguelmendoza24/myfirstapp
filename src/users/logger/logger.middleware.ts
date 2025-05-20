8f645c341b7d7225b1920e1162829ec7117c39b7
miguel <miguelevamendo@gmail.com>
2025-05-20 08:59:08 -0600

    middleware

--- src/users/logger/logger.middleware.ts
+++ src/users/logger/logger.middleware.ts
@@ -1,8 +1,10 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-
+import { Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  use(req: Request, res: Response, next: () => void) {
+    console.log('middleware', req.originalUrl);
+    
     next();
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Logger, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
+import path from 'path';
+import { AuthMiddleware } from './auth/auth.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+  
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(
+      { path: '/users', method: RequestMethod.GET },
+      {
+        path: '/users', method: RequestMethod.POST,
+      },
+    )
+      .apply(AuthMiddleware)
+    .forRoutes('users')
+  }
+}
