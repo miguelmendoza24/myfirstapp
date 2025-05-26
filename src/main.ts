56bf6d51cd6ff8605fc89ed658af0cf67cef43ad
miguel <miguelevamendo@gmail.com>
2025-05-26 09:28:38 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-
+app.enableCors()
 
   await app.listen(process.env.PORT ?? 3000);
 }
